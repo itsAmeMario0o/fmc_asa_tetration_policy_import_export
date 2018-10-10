0cfd8ad45267b59b7c652939d0481cc3df3d9570
Oxana Sannikova <oxsannikova@gmail.com>
2018-10-10 14:08:45 -0400

    adding cisco header

--- TetPolicy2.py
+++ TetPolicy2.py
@@ -1,3 +1,23 @@
+"""
+NOTE: This is a Proof of Concept script, please test before using in production!
+
+Copyright (c) 2018 Cisco and/or its affiliates.
+
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.0 (the "License"). You may obtain a copy of the
+License at
+
+               https://developer.cisco.com/docs/licenses
+
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+
+"""
+
 from tetpyclient import RestClient
 import json
 try:
@@ -5,6 +25,14 @@ try:
 except ImportError:
     Set = set
 from tqdm import tqdm
+from __future__ import absolute_import, division, print_function
+
+__author__ = "Chris Mchenry <chmchenr@cisco.com>"
+__contributors__ = [
+    "Oxana Sannikova <osanniko@cisco.com>"
+]
+__copyright__ = "Copyright (c) 2018 Cisco and/or its affiliates."
+__license__ = "Cisco Sample Code License, Version 1.0"
 
 class AbsolutePolicy(object):
     def __init__(self, policy):
